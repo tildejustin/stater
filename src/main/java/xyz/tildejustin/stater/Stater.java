58877d9b4e7dd5a26df02b27d937c1bb8ddc9981
tildejustin <spacepepper3.14@gmail.com>
2023-04-08 21:44:58 -0400

    added config file

--- src/main/java/xyz/tildejustin/stater/Stater.java
+++ src/main/java/xyz/tildejustin/stater/Stater.java
@@ -1,12 +1,16 @@
 package xyz.tildejustin.stater;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.spongepowered.include.com.google.gson.Gson;
+import org.spongepowered.include.com.google.gson.GsonBuilder;
 
-import java.util.logging.LogManager;
+import java.io.*;
 import java.util.logging.Logger;
 
 public class Stater implements ModInitializer {
-	static Logger LOGGER = Logger.getLogger("state");
+	static Logger LOGGER = Logger.getLogger("stater");
+	static File CONFIG_FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "stater.json");
 	public static long log_interval = 1000;
 
 	public static void log(String message) {
@@ -15,6 +19,43 @@ public class Stater implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		log_interval = 50;
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		try {
+			if (CONFIG_FILE.createNewFile()) {
+				try (Writer writer = new FileWriter(CONFIG_FILE)) {
+					gson.toJson(new Config(log_interval), writer);
+					log("wrote config file");
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		BufferedReader bufferedReader;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(CONFIG_FILE));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		Config config = gson.fromJson(bufferedReader, Config.class);
+		Stater.log_interval = config.getInterval();
+		log("read config file, log interval set to " + config.getInterval());
+
+	}
+
+	static class Config {
+		private final long interval;
+
+		@SuppressWarnings("unused")
+		Config() {
+			this.interval = Stater.log_interval;
+		}
+
+		Config(long interval) {
+			this.interval = interval;
+		}
+
+		public long getInterval() {
+			return interval;
+		}
 	}
 }
